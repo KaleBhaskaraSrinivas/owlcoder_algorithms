beeff482713a4d5ff96003da8c7d2b62905844d8
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-04-11 18:31:02 +0530

    Create Recursion subsequence by using target as sum.cpp

---
+++ Recursion subsequence by using target as sum.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void PrintSubSeqs(int ind,int n, vector<int>&ds, int arr[]){
+if(ind==n){
+for(auto it : ds){
+cout<<it<<" ";
+}
+  cout<<"\n";
+  return;
+}
+  //logic to make a pick
+  ds.push_back(arr[ind]);
+  PrintSubSeqs(ind+1,n,ds,arr);
+  ds.pop_back();
+  //logic to not to pick
+  PrintSubSeqs(ind+1,n,ds,arr);
+}
+
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  vector<int>ds;
+  PrintSubSeqs(0,n,ds,arr);
+}
