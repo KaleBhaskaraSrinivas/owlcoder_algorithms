ec88905c493685f79720d57268ac384f5a06ff3b
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-04-11 18:42:48 +0530

    Update Recursion subsequence by using target as sum.cpp

--- Recursion subsequence by using target as sum.cpp
+++ Recursion subsequence by using target as sum.cpp
@@ -1,28 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-void PrintSubSeqs(int ind,int n, vector<int>&ds, int arr[]){
+void PrintSubSeqs(int sum,int ind,int t,int n, vector<int>&ds, int arr[]){
 if(ind==n){
+    if (sum==t){
 for(auto it : ds){
 cout<<it<<" ";
+}
 }
   cout<<"\n";
   return;
 }
   //logic to make a pick
   ds.push_back(arr[ind]);
-  PrintSubSeqs(ind+1,n,ds,arr);
+  sum+=arr[ind];
+  PrintSubSeqs(sum,ind+1,t,n,ds,arr);
   ds.pop_back();
+  sum-=arr[ind];
   //logic to not to pick
-  PrintSubSeqs(ind+1,n,ds,arr);
+  PrintSubSeqs(sum,ind+1,t,n,ds,arr);
 }
 
 int main(){
-  int n;
-  cin>>n;
+  int n,t;
+  cin>>n>>t;
   int arr[n];
+  int sum=0;
   for(int i=0;i<n;i++){
     cin>>arr[i];
   }
   vector<int>ds;
-  PrintSubSeqs(0,n,ds,arr);
+  PrintSubSeqs(sum,0,t,n,ds,arr);
 }
