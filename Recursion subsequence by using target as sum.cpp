35aa2c63d4ee84473328cc6c153d2017dd41dfee
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-04-12 18:39:44 +0530

    Update Recursion subsequence by using target as sum.cpp

--- Recursion subsequence by using target as sum.cpp
+++ Recursion subsequence by using target as sum.cpp
@@ -31,3 +31,33 @@ int main(){
   PrintSubSeqs(0,0,t,n,ds,arr);
 }
 Here sum is taken as 0
+    
+//Another approach  
+#include<bits/stdc++.h>
+using namespace std;
+bool PrintSubSeqs(int sum,int ind,int t,int n, int arr[]){
+if(ind==n){
+    if (sum==t) return true;
+    else return false;
+}
+  //logic to make a pick
+ 
+  sum+=arr[ind];
+  if(PrintSubSeqs(sum,ind+1,t,n,arr) == true)return true;
+  sum-=arr[ind];
+  //logic to not to pick
+  if(PrintSubSeqs(sum,ind+1,t,n,arr) == true)return true;
+  return false;
+}
+
+int main(){
+  int n,t;
+  cin>>n>>t;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  if(PrintSubSeqs(0,0,t,n,arr)==true) cout<<"yes";
+  else cout<<"No";
+}
+
